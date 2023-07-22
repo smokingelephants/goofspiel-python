4bf6f6b5a81bf5782fe5fdbb5e21fa889ef862d1
T.J. Gaffney <gaffney.tj@gmail.com>
2023-07-22 12:00:05 -0700

    Add GoofLogger

--- scripts/run_bots_only.py
+++ scripts/run_bots_only.py
@@ -9,5 +9,6 @@ import goofspiel
 config = goofspiel.GameConfig(
     deck=[c for c in range(1, 4)],
     namer=bot_namer.NumberedNamer(),
+    logger=goofspiel.GoofLogger(),
 )
 goofspiel.play_game(n_bots=4, w_human=False, config=config)
--- src/goofspiel.py
+++ src/goofspiel.py
@@ -2,8 +2,9 @@
 
 This should be called from some other program."""
 
+import logging
 import random
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 import attr
 
@@ -24,10 +25,50 @@ class Player(object):
         raise NotImplementedError
 
 
+# TOOD: Move to another file
+class GoofErrors(Exception):
+    """Any error that's specific to this program's code."""
+    pass
+
+
+# TODO: Move above?
+LogTypes = str
+
+class GoofLogger(object):
+    _KNOWN_LOG_TYPES: Tuple[LogTypes, ...] = ("BIDS",)
+
+    def __init__(self, logger: Optional[logging.Logger] = None, log_types: Optional[List[LogTypes]] = None):
+        """This class will write messages to logger at logging level INFO if the 
+        message type is passed in log_types."""
+        self.logger = logger
+        if not self.logger:
+            self.logger = logging.getLogger("default_goof_logger")
+            self.logger.setLevel(logging.INFO)
+            console_handler = logging.StreamHandler()
+            console_handler.setLevel(logging.INFO)
+            self.logger.addHandler(console_handler)
+
+        if log_types is None:
+            self.log_types = list(self._KNOWN_LOG_TYPES)
+        else:
+            self.log_types = log_types[:]
+        for t in self.log_types:
+            if t not in self._KNOWN_LOG_TYPES:
+                raise GoofErrors(f"Encountered unknown log_type: {t}")
+
+    def bids(self, player_bids: List[Tuple[Player, Card]]) -> None:
+        if not "BIDS" in self.log_types:
+            return
+
+        for player, card in player_bids:
+            self.logger.info(f"{player.name} played {card}")
+
+
 @attr.s()
 class GameConfig(object):
     deck: List[Card] = attr.ib()
     namer: bot_namer.BotNamer = attr.ib()
+    logger: GoofLogger = attr.ib()
 
 
 class BotPlayer(Player):
@@ -51,8 +92,7 @@ def play_game_players(players: List[Player], config: GameConfig) -> None:
 
         # TODO: Add scoring part
 
-        # TODO: Pass logger
-        print(player_bids)
+        config.logger.bids(player_bids)
 
 
 def play_game(n_bots: int, w_human: bool, config: GameConfig) -> None:
